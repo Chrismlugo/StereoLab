abd156a5e770e983cfe4c3e9e6b00bde3080d237
Chris Murphy <_chrism_@live.com>
2018-03-06 10:43:10 +0000

    SteroLab Project created. adds Radio test Class

---
+++ src/test/java/TestRadio.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestRadio {
+    Radio radio;
+
+    @Before
+    public void setUp() throws Exception {
+        radio = new Radio();
+    }
+
+    @Test
+    public void test() {
+    }
+}
